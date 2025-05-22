014cb3f3e8e45c8a533056398ff99362430965e7
StoyanChorbov <slazy06bg@gmail.com>
2025-05-22 21:39:55 +0300

    Huh

--- Application/Controllers/HomeController.cs
+++ Application/Controllers/HomeController.cs
@@ -7,5 +7,5 @@ namespace Application.Controllers;
 public class HomeController : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get() => Ok("Great!");
+    public IActionResult Get() => Ok("Huh!");
 }
